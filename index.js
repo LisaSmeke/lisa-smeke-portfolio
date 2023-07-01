c51f6a01e7c75db26cabab98619db1ffdb9565a8
Lisa Smeke <lisasmeke@gmail.com>
2023-07-01 19:24:52 +0200

    Add toggle function to navbar hamburger menu btn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("Hello World, I'm Lisa");
-console.log(typeof 'Lisa');
+// console.log("Hello World, I'm Lisa");
+// console.log(typeof 'Lisa');
+
+const menuBtn = document.getElementById('menu-btn');
+const navList = document.getElementById('nav-list');
+
+menuBtn.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
