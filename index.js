a6643b9386a653792ce32d6d2f5de2bb25f5c079
Lisa Smeke <lisasmeke@gmail.com>
2023-07-02 14:08:22 +0200

    Change header color when scrolling

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ const navList = document.getElementById('nav-list');
 menuBtn.addEventListener('click', () => {
   navList.classList.toggle('active');
 });
+
+/*Change header color when scrolling*/
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
